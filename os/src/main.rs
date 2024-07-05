baa27cb9b8ca8b2016afabf5fda9abb234eea346
liheyu <1246299550@qq.com>
2024-07-05 16:21:46 +0800

    mini update

--- os/src/main.rs
+++ os/src/main.rs
@@ -30,50 +30,61 @@ global_asm!(include_str!("entry.asm"));
 /// clear BSS segment
 pub fn clear_bss() {
     extern "C" {
-        fn sbss();
-        fn ebss();
+        static sbss: u8;
+        static ebss: u8;
     }
-    (sbss as usize..ebss as usize).for_each(|a| unsafe { (a as *mut u8).write_volatile(0) });
+    unsafe {
+        // 获取BSS段的起始地址和结束地址
+        let sbss_ptr = &sbss as *const u8 as usize;
+        let ebss_ptr = &ebss as *const u8 as usize;
+
+        // 创建一个范围迭代器
+        (sbss_ptr..ebss_ptr).for_each(|addr| {
+            // 将地址转换为可变指针，并写入0
+            (addr as *mut u8).write_volatile(0);
+        });
+    }
 }
 
 /// the rust entry-point of os
 #[no_mangle]
 pub fn rust_main() -> ! {
     extern "C" {
-        fn stext(); // begin addr of text segment
-        fn etext(); // end addr of text segment
-        fn srodata(); // start addr of Read-Only data segment
-        fn erodata(); // end addr of Read-Only data ssegment
-        fn sdata(); // start addr of data segment
-        fn edata(); // end addr of data segment
-        fn sbss(); // start addr of BSS segment
-        fn ebss(); // end addr of BSS segment
-        fn boot_stack_lower_bound(); // stack lower bound
-        fn boot_stack_top(); // stack top
+        static stext: u64; // begin addr of text segment
+        static etext: u64; // end addr of text segment
+        static srodata: u64; // start addr of Read-Only data segment
+        static erodata: u64; // end addr of Read-Only data ssegment
+        static sdata: u64; // start addr of data segment
+        static edata: u64; // end addr of data segment
+        static sbss: u64; // start addr of BSS segment
+        static ebss: u64; // end addr of BSS segment
+        static boot_stack_lower_bound: u64; // stack lower bound
+        static boot_stack_top: u64; // stack top
     }
     clear_bss();
     logging::init();
-    println!("[kernel] Hello, world!");
-    trace!(
-        "[kernel] .text [{:#x}, {:#x})",
-        stext as usize,
-        etext as usize
-    );
-    debug!(
-        "[kernel] .rodata [{:#x}, {:#x})",
-        srodata as usize, erodata as usize
-    );
-    info!(
-        "[kernel] .data [{:#x}, {:#x})",
-        sdata as usize, edata as usize
-    );
-    warn!(
-        "[kernel] boot_stack top=bottom={:#x}, lower_bound={:#x}",
-        boot_stack_top as usize, boot_stack_lower_bound as usize
-    );
-    error!("[kernel] .bss [{:#x}, {:#x})", sbss as usize, ebss as usize);
-
+    println!("[kernel] \x1b[31mhello world!\x1b[0m");
+    unsafe {
+        trace!(
+            "[kernel] .text [{:#x}, {:#x})",
+            stext as usize,
+            etext as usize
+        );
+        debug!(
+            "[kernel] .rodata [{:#x}, {:#x})",
+            srodata as usize, erodata as usize
+        );
+        info!(
+            "[kernel] .data [{:#x}, {:#x})",
+            sdata as usize, edata as usize
+        );
+        warn!(
+            "[kernel] boot_stack top=bottom={:#x}, lower_bound={:#x}",
+            boot_stack_top as usize, boot_stack_lower_bound as usize
+        );
+        error!("[kernel] .bss [{:#x}, {:#x})", sbss as usize, ebss as usize);
+    }
     use crate::board::QEMUExit;
-    crate::board::QEMU_EXIT_HANDLE.exit_success(); // CI autotest success
-                                                   //crate::board::QEMU_EXIT_HANDLE.exit_failure(); // CI autoest failed
+    board::QEMU_EXIT_HANDLE.exit_success(); // CI autotest success
+                                            // board::QEMU_EXIT_HANDLE.exit_failure(); // CI autoest failed
 }
